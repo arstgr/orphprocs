5f3d1443c5557d755be26c33bb60922158179f9b
Mathieu Gontier <78465331+arstgr@users.noreply.github.com>
2022-04-19 19:56:37 +0000

    added error checks for pssh on the headnode and ssh connection to the hosts

--- nhc_multi_node_orphprocs.py
+++ nhc_multi_node_orphprocs.py
@@ -42,13 +42,38 @@ def find_VMs():
 
     return vmnames
 
+# checkif pssh is installed, if not then install pssh
+def check_pssh():
+    o, e = subprocess.getstatusoutput('pssh --version')
+    if o != 0:
+        os.system('sudo yum install pssh -y')
+
+#checks if the hosts accept SSH, if not, it removes them from the host list
+def check_VMs_ssh():
+    with open('hosts.txt') as f:
+        hosts = f.read().splitlines()
+
+    cmd = "pssh -p 194 -t 0 -i -h hosts.txt 'echo $(hostname)' | grep 'FAILURE' | awk '{print $4}'"
+    procs = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    o, e = procs.communicate()
+    o = o.decode('ascii')
+    o = re.split('[, \t \n]',o)
+    failed_hosts = list(filter(('').__ne__,o))
+    failed_hosts = list(set(failed_hosts))
+
+    hosts = [i for i in hosts if i not in failed_hosts]
+
+    with open('hosts.txt', 'w') as f:
+        for i in hosts:
+            f.write("%s\n" % i)
+
+    return failed_hosts
 
 # checks the cpu load for a all the available VMs (currently only pbs, should be extended to slurm as well) 
 # reports the process taking up the most of cpu on each VM
 # currently has no error handling, pssh may return an error (i.e. VM not responding) which should be handled properly
 # output is a dictionary
 def multi_VM_inst_test():
-    os.system('sudo yum install pssh -y')
     output = {}
     cmd = "pssh -p 194 -t 0 -i -h hosts.txt 'sudo ps -Ao user,uid,comm,pid,ppid,pcpu,pmem --sort=-%cpu | head -n 2' | awk -F ' ' '{print $4; getline; getline; print $1, $2, $3, $4, $5, $6, $7}'"
     procs = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -73,7 +98,6 @@ def multi_VM_inst_test():
 # currently has no error handling, pssh may return an error (i.e. VM not responding) which should be handled properly
 # output is added to the result dictionary
 def multi_VM_uptime_test(results):
-    os.system('sudo yum install pssh -y')
     cmd = "pssh -p 194 -t 0 -i -h hosts.txt uptime | awk -F ' ' '{print $4 $10 $11 $12}'"
     procs = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     o, e = procs.communicate()
@@ -91,7 +115,7 @@ def multi_VM_uptime_test(results):
     
     return results #output
 
-def check_VM_load(results):
+def check_VM_load(results, failed_hosts):
     for i in results.keys():
         if float(results[i]['5min']) >= float(AVG_TRSHLD) and float(results[i]['1min']) >= float(AVG_TRSHLD) and float(results[i]['CPU%']) >= float(INST_TRSHLD):
             results[i]['STATUS'] = 'FAILED'
@@ -101,6 +125,10 @@ def check_VM_load(results):
             results[i]['STATUS'] = 'TRANSIENT'
         else:
             results[i]['STATUS'] = 'PASSED'
+
+    for i in failed_hosts:
+        results[i] = {}
+        results[i]['STATUS'] = 'SSH FAILED' 
 
     return results
 
@@ -112,26 +140,22 @@ def summarize_VM_load(results):
     return summary
 
 ############################################################################################
-
+check_pssh()
 outputlist = find_VMs()
-#print(outputlist)
+
+failed_hosts = check_VMs_ssh()
 
 results = {}
 results = multi_VM_inst_test()
 
 results = multi_VM_uptime_test(results)
-results = check_VM_load(results)
+results = check_VM_load(results, failed_hosts)
 summary = summarize_VM_load(results)
-
-#print("Test Results")
-#json_results = json.dumps(results, indent = 4)
-#print(json_results)
 
 print("Test Summary")
 json_summary = json.dumps(summary, indent = 4)
 print(json_summary)
 
-#outputs the test results into a json file
 with open("VM_loads.json", "w") as outfile:
         json.dump(results, outfile, indent = 4)
 
